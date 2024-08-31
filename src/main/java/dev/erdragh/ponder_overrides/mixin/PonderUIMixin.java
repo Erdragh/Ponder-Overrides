4dba334588f02f0f3f41697dc91f55b4c3710685
Erdragh <mail@erdragh.dev>
2024-08-31 15:47:49 +0200

    Only debug log in mixin

--- src/main/java/dev/erdragh/ponder_overrides/mixin/PonderUIMixin.java
+++ src/main/java/dev/erdragh/ponder_overrides/mixin/PonderUIMixin.java
@@ -4,6 +4,8 @@ import dev.erdragh.ponder_overrides.kube.PonderOverrideEventJS;
 
 import dev.erdragh.ponder_overrides.PonderOverrides;
 
+import net.minecraft.world.item.ItemStack;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,14 +14,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.simibubi.create.foundation.ponder.PonderTag;
 import com.simibubi.create.foundation.ponder.ui.PonderUI;
 
-import net.minecraft.world.item.ItemStack;
-
 @Mixin(PonderUI.class)
 public class PonderUIMixin {
-
 	@Inject(method = "of(Lnet/minecraft/world/item/ItemStack;)Lcom/simibubi/create/foundation/ponder/ui/PonderUI;", at = @At("HEAD"), cancellable = true)
 	private static void nbt_ponders$of(ItemStack item, CallbackInfoReturnable<PonderUI> cir) {
-		PonderOverrides.LOGGER.info("pondering {}", item);
+		PonderOverrides.LOGGER.debug("pondering {}", item);
 		var event = new PonderOverrideEventJS(item);
 		event.post();
 		event.getOverride().map(PonderUI::of).ifPresent(cir::setReturnValue);
@@ -27,7 +26,7 @@ public class PonderUIMixin {
 
 	@Inject(method = "of(Lnet/minecraft/world/item/ItemStack;Lcom/simibubi/create/foundation/ponder/PonderTag;)Lcom/simibubi/create/foundation/ponder/ui/PonderUI;", at = @At("HEAD"), cancellable = true)
 	private static void nbt_ponders$of(ItemStack item, PonderTag tag, CallbackInfoReturnable<PonderUI> cir) {
-		PonderOverrides.LOGGER.info("pondering {} with tag {}", item, tag);
+		PonderOverrides.LOGGER.debug("pondering {} with tag {}", item, tag);
 		var event = new PonderOverrideEventJS(item);
 		event.post();
 		event.getOverride().map(PonderUI::of).ifPresent(ui -> {
